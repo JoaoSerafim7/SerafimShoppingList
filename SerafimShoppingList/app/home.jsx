10224bc8c867b1c17919620f7ce7e8311ca2a87a
JoaoSerafim7 <joaoserafim1107@gmail.com>
2025-05-08 12:27:47 -0300

    galao do disiguine

--- SerafimShoppingList/app/home.jsx
+++ SerafimShoppingList/app/home.jsx
@@ -1,12 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Ionicons } from '@expo/metro-runtime'
 
 export default function Home() {
   return (
-    <View>
-      <Text>ome</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1}}>
+      <ImageBackground
+        source={require('../assets/background.jpg')}
+        style={{ flex: 1, justifyContent: 'flex-start' }}
+        resizeMode='repeat'
+      >
+        <View style={styles.header}>
+          <Text style={styles.title}>Lista de Produtos</Text>
+          <Ionicons name="trash" size={32} color="#fff" />
+        </View>
+
+
+      </ImageBackground>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    padding: 25,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#000000c0',
+    borderBottomRightRadius: 30,
+    borderBottomLeftRadius: 30
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#fff'
+  }
+})
